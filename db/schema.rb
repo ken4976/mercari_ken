2ad9e6334d37d48315112f49ac05cdaa74f79016
森田賢 <moritaken@moritakennoMacBook-puro.local>
2018-10-20 16:13:34 +0900

    add_seeds

--- db/schema.rb
+++ db/schema.rb
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 20181017121627) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                                          null: false
+    t.string   "nickname"
     t.string   "email",                                default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
